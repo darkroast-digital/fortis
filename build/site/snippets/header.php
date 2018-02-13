84ffc5512248e487d80ad215b2c7417592bef107
Kim Morin <kimmorin93@gmail.com>
2018-02-13 15:42:17 -0500

    g.a. script added

--- build/site/snippets/header.php
+++ build/site/snippets/header.php
@@ -1,5 +1,53 @@
-<!doctype html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="x-ua-compatible" content="ie=edge"><!-- Viewport Meta --><meta name="viewport" content="width=device-width,initial-scale=1"><!-- Styles --><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"><link rel="stylesheet" href="assets/css/main.css"><!-- Mobile Theme Colour --><meta name="theme-color" content="#766dcc"><!-- Title and Description --><title>Fortis Group</title><meta name="description" content="Fortis Group is the innovative product of the merger between STC Contracting Group and DeAngelis Contracting Inc."><meta name="keywords" content="Fortis Group, STC Contracting, STC Construction, Dengelis Contracting, Dengelis Construction, construction, commercial, automotive, industrial, institutional, retail, restaurant"><!-- Favicons --><link rel="icon" href="https://res.cloudinary.com/darkroast-digital/image/upload/v1510152415/fortis/fav.png"><!-- Open Graph --><meta property="og:title" content="Fortis Group"><meta property="og:description" content="Fortis Group is the innovative product of the merger between STC Contracting Group and DeAngelis Contracting Inc."><meta property="og:type" content="website"><meta property="og:url" content="https://fortisgroup.ca"><meta property="og:site_name" content="Fortis Group"><meta property="og:image" content="https://res.cloudinary.com/darkroast-digital/image/upload/v1510152415/fortis/ogimage.jpg"><!--[if lt IE 9]>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-100160558-5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-100160558-5');
+</script>
+
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- Viewport Meta -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- Mobile Theme Colour -->
+    <meta name="theme-color" content="#766dcc">
+    <!-- Title and Description -->
+    <title>Fortis Group</title>
+    <meta name="description" content="Fortis Group is the innovative product of the merger between STC Contracting Group and DeAngelis Contracting Inc.">
+    <meta name="keywords" content="Fortis Group, STC Contracting, STC Construction, Dengelis Contracting, Dengelis Construction, construction, commercial, automotive, industrial, institutional, retail, restaurant">
+    <!-- Favicons -->
+    <link rel="icon" href="https://res.cloudinary.com/darkroast-digital/image/upload/v1510152415/fortis/fav.png">
+    <!-- Open Graph -->
+    <meta property="og:title" content="Fortis Group">
+    <meta property="og:description" content="Fortis Group is the innovative product of the merger between STC Contracting Group and DeAngelis Contracting Inc.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://fortisgroup.ca">
+    <meta property="og:site_name" content="Fortis Group">
+    <meta property="og:image" content="https://res.cloudinary.com/darkroast-digital/image/upload/v1510152415/fortis/ogimage.jpg">
+    <!--[if lt IE 9]>
      <script src="js/polyfill/html5-shiv.js"></script>
-  <![endif]--></head><body><!--[if IE]>
+  <![endif]-->
+</head>
+
+<body>
+    <!--[if IE]>
       <div class="browserupgrade"><p>You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience.</p></div>
-   <![endif]--><div class="loader"><div class="spinner-container"><div class="spinner"></div></div></div></body></html>
+   <![endif]-->
+    <div class="loader">
+        <div class="spinner-container">
+            <div class="spinner"></div>
+        </div>
+    </div>
+</body>
+
+</html>
